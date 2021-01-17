4a0efcccb19a7d24e31d44ca71a72d542a289157
Harsh Vardhan Bahree <harshvardhanbahree@gmail.com>
2021-01-18 02:13:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ function App() {
   return (
     <div>
         <h1>{process.env.REACT_APP_SECRET_NAME}</h1>
+        <img src="https://earthsky.org/upl/2018/12/comet-wirtanen-Jack-Fusco-dec-2018-Anza-Borrego-desert-CA-e1544613895713.jpg">
+        </img>
       </div>
   );
 }
